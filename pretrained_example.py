38d20f129a4fb3c5340e8795a06da77a78b91145
Michele Toni <michele@checkoutfree.it>
2020-02-21 12:08:50 +0100

    Arguments from command line in pretrained_example.py

--- pretrained_example.py
+++ pretrained_example.py
@@ -7,17 +7,28 @@
 
 """Minimal script for generating an image using pre-trained StyleGAN generator."""
 
+import argparse
 import os
 import pickle
 import numpy as np
 import PIL.Image
-import dnnlib
 import dnnlib.tflib as tflib
-import config
+
+def do_parsing():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     description="Use trained model to generate images")
+    parser.add_argument("--model_checkpoint", required=True, type=str, help="Configuration file")
+    parser.add_argument("--output_dir", required=True, type=str,
+                        help="Output directory that will contain histograms and CSV file with results")
+    args = parser.parse_args()
+    return args
 
 def main():
+    args = do_parsing()
+    print(args)
+
     tflib.init_tf()
-    _G, _D, Gs = pickle.load(open("results/00001-sgan-logos-1gpu/network-snapshot-002364.pkl", "rb"))
+    _G, _D, Gs = pickle.load(open(args.model_checkpoint, "rb"))
     Gs.print_layers()
 
     for i in range(0,25):
@@ -25,8 +36,8 @@ def main():
         latents = rnd.randn(1, Gs.input_shape[1])
         fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
         images = Gs.run(latents, None, truncation_psi=0.6, randomize_noise=True, output_transform=fmt)
-        os.makedirs(config.result_dir, exist_ok=True)
-        png_filename = os.path.join(config.result_dir, 'example-'+str(i)+'.png')
+        os.makedirs(args.output_dir, exist_ok=True)
+        png_filename = os.path.join(args.output_dir, 'example-'+str(i)+'.png')
         PIL.Image.fromarray(images[0], 'RGB').save(png_filename)
 
 if __name__ == "__main__":
